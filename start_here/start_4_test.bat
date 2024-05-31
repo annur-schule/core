fb8a28f1fabb9e9ddcd1e314a66800f08007b952
wakas <wmun@gmx.de>
2024-05-31 04:02:33 +0200

    Test instance can now be started directly through the bat file in start_here folder Docker should be installed and running for this.

---
+++ start_here/start_4_test.bat
@@ -0,0 +1,18 @@
+@echo off
+setlocal
+
+if not exist Dockerfile (
+	echo Necessary files do not exist. Not running from correct folder?
+	pause
+	exit /b
+)
+
+docker image build -t annurschule/gibbon:php7.4 . 
+
+start /B docker-compose up
+
+rem create a delay
+ping 127.0.0.1 -n 6 > nul
+
+msg * "Now open localhost:8081 in your browser."
+
